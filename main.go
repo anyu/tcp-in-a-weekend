1b01c090116d889105889065c8eb08c89e236c54
An Yu <1009227+anyu@users.noreply.github.com>
2023-11-06 14:15:43 -0500

    wip udp

--- main.go
+++ main.go
@@ -198,7 +198,7 @@ func ipv4FromBytes(b []byte) (IPv4, error) {
 	return ipv4, nil
 }
 
-func getChecksum(data []byte) uint16 {
+func generateChecksum(data []byte) uint16 {
 	// add padding to ensure even number of bytes
 	if len(data)%2 == 1 {
 		data = append(data, 0x00)
@@ -246,7 +246,7 @@ func createIPv4(contentLength uint16, protocol uint8, destIP []byte, ttl uint8) 
 		src:         srcIP.To4(),
 		dest:        destIP,
 	}
-	ipv4.checksum = getChecksum(ipv4.toBytes())
+	ipv4.checksum = generateChecksum(ipv4.toBytes())
 	return ipv4
 }
 
@@ -347,4 +347,76 @@ func ping(ip string, count int) error {
 		fmt.Printf("response from: %s icmp_seq=%d ttl=%d time=%.3f ms\n", ip, response.Seq, replyIP.ttl, elapsedMS)
 	}
 	return nil
+}
+
+// UDP datagrams consist of a header followed by the payload.
+// The header contains of four 2-byte fields, totaling 8 bytes.
+type UDP struct {
+	// SrcPort is port of the device sending the data. Can be 0 if no reply is needed.
+	SrcPort uint16
+	// DestPort is the port of the device receiving the data.
+	DestPort uint16
+	// Length specifies the # of bytes comprising the header and payload.
+	Length uint16
+	// Checksum is used to verify the integrity of the packet. Optional in ipv4, required in ipv6.
+	// NOTE: The checksum includes part of the IP header, despite the IP header having its own checksum.
+	// This combination of the IP header, UDP header, and payload is called the pseudo header.
+	Checksum uint16
+	// Contents is the payload data.
+	Contents []byte
+}
+
+func (u *UDP) toBytes() []byte {
+	header := make([]byte, 8)
+	binary.BigEndian.PutUint16(header[0:2], u.SrcPort)
+	binary.BigEndian.PutUint16(header[2:4], u.DestPort)
+
+	binary.BigEndian.PutUint16(header[4:6], u.Length)
+	binary.BigEndian.PutUint16(header[6:8], u.Checksum)
+
+	return append(header, u.Contents...)
+}
+
+func udpFromBytes(data []byte) UDP {
+	udp := UDP{}
+
+	header := data[:8]
+	payload := data[8:]
+	udp.SrcPort = binary.BigEndian.Uint16(header[0:2])
+	udp.DestPort = binary.BigEndian.Uint16(header[2:4])
+	udp.Length = binary.BigEndian.Uint16(header[4:6])
+	udp.Checksum = binary.BigEndian.Uint16(header[6:8])
+	udp.Contents = payload
+	return udp
+}
+
+func genPseudoHeaderChecksum(ipv4 IPv4, payload []byte) uint16 {
+	ipv4PseudoHeader := make([]byte, 12)
+
+	copy(ipv4PseudoHeader[0:4], ipv4.src.To4())
+	copy(ipv4PseudoHeader[4:8], ipv4.dest.To4())
+
+	ipv4PseudoHeader[8] = 0 // technically the ToS field typically set to 0
+	ipv4PseudoHeader[9] = ipv4.protocol
+	binary.BigEndian.PutUint16(ipv4PseudoHeader[10:12], ipv4.totalLength-20)
+
+	pseudoHeader := append(ipv4PseudoHeader, payload...)
+
+	return generateChecksum(pseudoHeader)
+}
+
+func createUDP(ip net.IP, srcPort, destPort uint16, contents []byte) []byte {
+	udp := UDP{
+		SrcPort:  srcPort,
+		DestPort: destPort,
+		Length:   0,
+		Checksum: 0,
+		Contents: contents,
+	}
+
+	udpBytes := udp.toBytes()
+	ipv4 := createIPv4(uint16(len(udpBytes)), PROTO_UDP, ip, 64)
+	udp.Checksum = genPseudoHeaderChecksum(ipv4, udpBytes)
+
+	return append(ipv4.toBytes(), udp.toBytes()...)
 }
